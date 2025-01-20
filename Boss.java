e32f79bd8be1c165dd7c81813ac97ba7b6c19228
Jaden Bae <jbae50@stuy.edu>
2025-01-20 01:14:40 -0500

    Changed Boss Dialogue, special name, etc.

--- Boss.java
+++ Boss.java
@@ -1,65 +1,65 @@
 public class Boss extends Adventurer{
-  int anger, angerMax;
+  int intoxication, intoxicationMax;
   String preferredWeapon;
 
   /*the other constructors ultimately call the constructor
   *with all parameters.*/
   public Boss(String name, int hp, String weapon){
     super(name,hp);
-    angerMax = 12;
-    anger = angerMax/2;
+    intoxicationMax = 55;
+    intoxication = intoxicationMax/2;
     preferredWeapon = weapon;
   }
 
   public Boss(String name, int hp){
-    this(name,hp,"Baseball Bat");
+    this(name,hp,"Broken Bottle");
   }
 
   public Boss(String name){
-    this(name,30);
+    this(name,100);
   }
 
   public Boss(){
-    this("Boss");
+    this("Drunkard");
   }
 
   /*The next 8 methods are all required because they are abstract:*/
   public String getSpecialName(){
-    return "anger";
+    return "intoxication";
   }
 
   public int getSpecial(){
-    return anger;
+    return intoxication;
   }
 
   public void setSpecial(int n){
-    anger = n;
+    intoxication = n;
   }
 
   public int getSpecialMax(){
-    return angerMax;
+    return intoxicationMax;
   }
 
-  /*Deal 2-7 damage to opponent, restores 2 anger*/
+  /*Deal 2-7 damage to opponent, restores 2 intoxication*/
   public String attack(Adventurer other){
     int damage = (int)(Math.random()*6)+2;
     other.applyDamage(damage);
     restoreSpecial(2);
-    return this + " attacked "+ other + " and dealt "+ damage +
-    " points of damage. He then lets out a thick roar.";
+    return "Heh, gotcha, didn’t I? Just... just don’t make me swing again! "+ this + " striked their "+preferredWeapon+ " and he knocked out "+ other + " and dealt "+ damage +
+    " points of damage.";
   }
 
-  /*Deal 3-12 damage to opponent, only if anger is high enough.
-  *Reduces anger by 8.
+  /*Deal 3-12 damage to opponent, only if intoxication is high enough.
+  *Reduces intoxication by 8.
   */
   public String specialAttack(Adventurer other){
     if(getSpecial() >= 8){
       setSpecial(getSpecial()-8);
       int damage = (15);
       other.applyDamage(damage);
-      return this + " striked their "+preferredWeapon+ " to " + other + " He knocked out "+other+" dealing "+ damage +" points of damage.";
+      return "This is what you get for messin' with me, buddy! A little liquid courage for ya! " + this + " unzips his pants and aims to urinate toward "+ other +" , laughing uncontrollably, dealing "+ damage +" points of damage. Yuck!";
     }else{
-      return "Not enough anger to use the strike. Instead "+attack(other);
+      return "How about bit of this? "+attack(other);
     }
 
   }
@@ -73,7 +73,7 @@ public class Boss extends Adventurer{
   public String support(){
     int hp = 1;
     setHP(getHP()+hp);
-    return this+" yells out to restore"+restoreSpecial((int)(Math.random()*1)+1)+" "
+    return "Yeah, I'm definitely ready for this. Let's get crazy, shall we? "+ this+" yells out to restore"+restoreSpecial((int)(Math.random()*1)+1)+" "
     + getSpecialName()+ " and "+hp+" HP";
   }
 }
