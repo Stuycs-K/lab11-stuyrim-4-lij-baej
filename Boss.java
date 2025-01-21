518a7cde95ab454db4407ae6b088705432275d01
Jaden Bae <jbae50@stuy.edu>
2025-01-20 21:37:11 -0500

    created boss's DOT damage skill

--- Boss.java
+++ Boss.java
@@ -1,6 +1,10 @@
+import java.util.*;
+
 public class Boss extends Adventurer{
   int intoxication, intoxicationMax;
   String preferredWeapon;
+
+  ArrayList<Adventurer>sprayList = new ArrayList<Adventurer>();
 
   /*the other constructors ultimately call the constructor
   *with all parameters.*/
@@ -40,8 +44,17 @@ public class Boss extends Adventurer{
     return intoxicationMax;
   }
 
+  public void dealDotDamage(){
+    int damage = (4);
+
+    for (Adventurer member : sprayList){
+      member.applyDamage(damage);
+    }
+  }
+
   /*Deal 2-7 damage to opponent, restores 2 intoxication*/
   public String attack(Adventurer other){
+    dealDotDamage();
     int damage = (int)(Math.random()*6)+2;
     other.applyDamage(damage);
     restoreSpecial(2);
@@ -54,10 +67,13 @@ public class Boss extends Adventurer{
   */
   public String specialAttack(Adventurer other){
     if(getSpecial() >= 8){
+      dealDotDamage();
       setSpecial(getSpecial()-8);
-      int damage = (15);
+      int damage = (8);
+
+      sprayList.add(other);
       other.applyDamage(damage);
-      return "This is what you get for messin' with me, buddy! A little liquid courage for ya! " + this + " unzips his pants and aims to urinate toward "+ other +", laughing uncontrollably, dealing "+ damage +" points of damage. Yuck!";
+      return "This is what you get for messin' with me, buddy! A little liquid courage for ya! " + this + " unzips his pants and aims to urinate toward "+ other +", laughing uncontrollably, dealing "+ damage +" points of damage."+ other +" is drenched, and will recieve 4 damage each turn. Yuck!";
     }else{
       return "How about bit of this? "+attack(other);
     }
@@ -65,12 +81,14 @@ public class Boss extends Adventurer{
   }
   /*Restores 5 special to other*/
   public String support(Adventurer other){
+    dealDotDamage();
     return "Gives a encouraging yell to "+other+" and restores "
     + other.restoreSpecial((int)(Math.random()*1)+1)+" "+other.getSpecialName();
   }
 
   /*Restores 6 special and 1 hp to self.*/
   public String support(){
+    dealDotDamage();
     int hp = 1;
     setHP(getHP()+hp);
     return "I’m feeling it now. Nothing can slow me down after a drink like that! Let's get crazy, shall we? "+ this+" chugs gallon of whisky to restore "+restoreSpecial((int)(Math.random()*1)+1)+" "
