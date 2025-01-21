3ed30c6020700e7099015cab229fe2036980aa2f
Jaden Bae <jbae50@stuy.edu>
2025-01-21 00:54:41 -0500

    fix for thug's dialogues

--- Thug.java
+++ Thug.java
@@ -1,13 +1,13 @@
 public class Thug extends Adventurer{
-  int anger, angerMax;
+  int charisma, charismaMax;
   String preferredWeapon;
 
   /*the other constructors ultimately call the constructor
   *with all parameters.*/
   public Thug(String name, int hp, String weapon){
     super(name,hp);
-    angerMax = 10;
-    anger = angerMax/2;
+    charismaMax = 10;
+    charisma = charismaMax/2;
     preferredWeapon = weapon;
   }
 
@@ -25,22 +25,22 @@ public class Thug extends Adventurer{
 
   /*The next 8 methods are all required because they are abstract:*/
   public String getSpecialName(){
-    return "anger";
+    return "charisma";
   }
 
   public int getSpecial(){
-    return anger;
+    return charisma;
   }
 
   public void setSpecial(int n){
-    anger = n;
+    charisma = n;
   }
 
   public int getSpecialMax(){
-    return angerMax;
+    return charismaMax;
   }
 
-  /*Deal 2-7 damage to opponent, restores 2 anger*/
+  /*Deal 2-7 damage to opponent, restores 2 charisma*/
   public String attack(Adventurer other){
     int damage = (int)(multiplier() * (int)(Math.random()*6)+2);
     other.applyDamage(damage);
@@ -52,12 +52,12 @@ public class Thug extends Adventurer{
         setRounds(0);
       }
     }
-    return this + " attacked "+ other + " and dealt "+ damage +
-    " points of damage. He then lets out a thick roar.";
+    return "Take this! " + this + " attacked "+ other + " and dealt "+ damage +
+    " points of damage. ";
   }
 
-  /*Deal 3-12 damage to opponent, only if anger is high enough.
-  *Reduces anger by 8.
+  /*Deal 3-12 damage to opponent, only if charisma is high enough.
+  *Reduces charisma by 8.
   */
   public String specialAttack(Adventurer other){
     if(getSpecial() >= 8){
@@ -71,15 +71,15 @@ public class Thug extends Adventurer{
           setRounds(0);
         }
       }
-      return this + " striked their "+preferredWeapon+ " to " + other + " He knocked out "+other+" dealing "+ damage +" points of damage.";
+      return "Watch out for my pookie! "+ this + " striked their "+preferredWeapon+ " to " + other + " He knocked out "+other+" dealing "+ damage +" points of damage.";
     }else{
-      return "Not enough anger to use the strike. Instead "+attack(other);
+      return "Not enough charisma to use the strike. Instead "+attack(other);
     }
 
   }
   /*Restores 5 special to other*/
   public String support(Adventurer other){
-    return "Gives a encouraging yell to "+other+" and restores "
+    return this + " gives a encouraging yell to "+other+" and restores "
     + other.restoreSpecial((int)(Math.random()*1)+1)+" "+other.getSpecialName();
   }
 
@@ -87,7 +87,7 @@ public class Thug extends Adventurer{
   public String support(){
     int hp = 1;
     setHP(getHP()+hp);
-    return this+" yells out to restore "+restoreSpecial((int)(Math.random()*1)+1)+" "
+    return this+" lifts his collar and fixes his fit to restore "+restoreSpecial((int)(Math.random()*1)+1)+" "
     + getSpecialName()+ " and "+hp+" HP";
   }
 }
